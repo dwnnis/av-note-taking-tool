4490a86ad4b2f4497f9bec756034c7603b81241c
dennisw2 <dennisw2@illinois.edu>
2020-03-09 15:56:02 -0500

    * change video portion

--- video.js
+++ video.js
@@ -41,6 +41,7 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
+    height: '100%',
     width: '100%',
     videoId: 'BiySUpmBUCo',
     events: {
