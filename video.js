450d9f127b99aa7991ec89502fc6d737bcbbcb0e
dennisw2 <dennisw2@illinois.edu>
2020-03-09 15:33:34 -0500

    * add recording function* add recording function* add recording function* add recording function* add recording function* add recording function* add recording function* add recording function

--- video.js
+++ video.js
@@ -1,3 +1,35 @@
+/*
+  Setups
+*/
+
+//webkitURL is deprecated but nevertheless
+URL = window.URL || window.webkitURL;
+
+var gumStream; 						//stream from getUserMedia()
+var rec; 							//Recorder.js object
+var input; 							//MediaStreamAudioSourceNode we'll be recording
+
+// shim for AudioContext when it's not avb.
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var audioContext; //audio context to help us record
+
+
+var recordButton = document.getElementById("recordButton");
+var stopButton = document.getElementById("stopButton");
+var timestampButton = document.getElementById("timestampButton");
+var unmuteButton = document.getElementById("unmuteButton");
+var addElement = document.getElementById("addElementButton");
+
+recordButton.addEventListener("click", startRecording);
+stopButton.addEventListener("click", stopRecording);
+timestampButton.addEventListener("click", updatetime);
+unmuteButton.addEventListener("click", unmute);
+addElement.addEventListener("click", addElement);
+
+/*
+  Official Example of YouTube iframe API
+*/
+
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
@@ -38,10 +70,17 @@ function stopVideo() {
   player.stopVideo();
 }
 
+
+/*
+  Button Calls
+*/
+
 // TIMESTAMP: listen to button and change html
 var timestamp = document.getElementById("timestamp");
+var currentTimestamp;
 function updatetime() {
-  timestamp.innerHTML = player.getCurrentTime();
+  currentTimestamp = player.getCurrentTime();
+  timestamp.innerHTML = currentTimestamp;
 }
 
 // SCREENSHOT: listen to button and take screenshot
@@ -63,4 +102,98 @@ function unmute(){
   player.unMute();
 }
 
+// ADD ITEM: add new item
+var counter = 0;
+function addElement() {
+  var newDiv = document.createElement("div");
+  var newId = "div".concat(counter.toString());
+  newDiv.setAttribute("id", newId);
+
+  var newContent = document.createTextNode("Hi there and greetings!".concat(counter.toString()));
+  newDiv.appendChild(newContent);
+
+  var previousDiv = document.getElementById("div".concat((counter-1).toString()));
+  if (counter == 0) {
+    document.body.insertBefore(newDiv, document.getElementById("note-list"));
+  } else if (counter != 0) {
+    document.body.insertBefore(newDiv, previousDiv);
+  }
+  counter += 1;
+}
+
 // VOICE RECORDER: 1) pause video 2) record 3)
+
+function startRecording() {
+  currentTimestamp = player.getCurrentTime();
+	console.log("recordButton clicked");
+
+  var constraints = { audio: true, video:false }
+
+  recordButton.disabled = true;
+	stopButton.disabled = false;
+
+  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+    console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
+
+    audioContext = new AudioContext();
+    gumStream = stream;
+    input = audioContext.createMediaStreamSource(stream);
+
+    rec = new Recorder(input,{numChannels:1});
+    rec.record()
+
+    console.log("Recording started");
+
+  }).catch(function(err) {
+      //enable the record button if getUserMedia() fails
+      console.log("failed start recording");
+      console.log(err);
+      recordButton.disabled = false;
+      stopButton.disabled = true;
+  });
+}
+
+function stopRecording() {
+	console.log("stopButton clicked");
+
+	//disable the stop button, enable the record too allow for new recordings
+	stopButton.disabled = true;
+	recordButton.disabled = false;
+
+	rec.stop();
+	gumStream.getAudioTracks()[0].stop();
+
+	rec.exportWAV(createDownloadLink);
+}
+
+function createDownloadLink(blob) {
+
+	var url = URL.createObjectURL(blob);
+	var au = document.createElement('audio');
+  var navBtn = document.createElement('button')
+	var li = document.createElement('li');
+	var link = document.createElement('a');
+
+	//name of .wav file to use during upload and download (without extendion)
+	var filename = new Date().toLocaleString("en-US", {timeZone: "America/Chicago"});
+
+	//add controls to the <audio> element
+	au.controls = true;
+	au.src = url;
+
+  //button to the timestamp
+  navBtn.innerHTML = currentTimestamp.toString();
+  navBtn.setAttribute("onclick", "navigateVideoAtTime("+currentTimestamp.toString()+")");
+
+	//save to disk link
+	link.href = url;
+	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
+	link.innerHTML = "Save to disk";
+
+	//add the new audio element to li
+	li.appendChild(au);
+  li.appendChild(navBtn);
+	li.appendChild(document.createTextNode(filename+".wav "))
+	li.appendChild(link);
+	recordingsList.appendChild(li);
+}
